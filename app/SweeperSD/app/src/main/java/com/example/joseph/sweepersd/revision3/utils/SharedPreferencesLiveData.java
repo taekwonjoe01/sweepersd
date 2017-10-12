91e356bba90e3bbd23ec77aed586b4e62aefbc34
Joseph Hutchins <jhu@ciklum.com>
2017-10-12 12:08:03 -0700

    SharedPreferences LiveData.

--- app/SweeperSD/app/src/main/java/com/example/joseph/sweepersd/revision3/utils/SharedPreferencesLiveData.java
+++ app/SweeperSD/app/src/main/java/com/example/joseph/sweepersd/revision3/utils/SharedPreferencesLiveData.java
@@ -1,8 +1,37 @@
 package com.example.joseph.sweepersd.revision3.utils;
 
-/**
- * Created by joseph on 10/11/17.
- */
+import android.arch.lifecycle.LiveData;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
-public class SharedPreferencesLiveData extends LiveData<> {
+public class SharedPreferencesLiveData extends LiveData<SharedPreferences> {
+    private final Context mContext;
+
+    private SharedPreferences mSharedPreferences;
+
+    private final SharedPreferences.OnSharedPreferenceChangeListener mSharedPreferencesListener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+            mSharedPreferences = sharedPreferences;
+            postValue(mSharedPreferences);
+        }
+    };
+
+    public SharedPreferencesLiveData(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    protected void onActive() {
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        mSharedPreferences.registerOnSharedPreferenceChangeListener(mSharedPreferencesListener);
+        setValue(mSharedPreferences);
+    }
+
+    @Override
+    protected void onInactive() {
+        mSharedPreferences.unregisterOnSharedPreferenceChangeListener(mSharedPreferencesListener);
+    }
 }
