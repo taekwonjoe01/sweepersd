b1508a1cd1d3a9fbec513c8d9f494152401900aa
Joseph Hutchins <joeyh89@vt.edu>
2017-11-20 13:24:54 -0800

    Update to support lower versions of android (lollipop).

--- app/SweeperSD/app/src/main/java/com/example/joseph/sweepersd/alert/AlertUpdater.java
+++ app/SweeperSD/app/src/main/java/com/example/joseph/sweepersd/alert/AlertUpdater.java
@@ -5,7 +5,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.example.joseph.sweepersd.R;
 import com.example.joseph.sweepersd.alert.geofence.WatchZoneFence;
