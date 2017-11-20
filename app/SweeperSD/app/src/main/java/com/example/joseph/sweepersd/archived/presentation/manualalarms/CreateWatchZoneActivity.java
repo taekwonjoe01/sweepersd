7fa7029401e290811e7db82a29320ab531a2b244
Joseph Hutchins <joeyh89@vt.edu>
2017-11-20 13:31:20 -0800

    cleanup resources.

--- app/SweeperSD/app/src/main/java/com/example/joseph/sweepersd/archived/presentation/manualalarms/CreateWatchZoneActivity.java
+++ app/SweeperSD/app/src/main/java/com/example/joseph/sweepersd/archived/presentation/manualalarms/CreateWatchZoneActivity.java
@@ -78,7 +78,7 @@ public class CreateWatchZoneActivity extends AppCompatActivity implements OnMapR
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        mRadiusDisplay = (TextView) findViewById(R.id.radius_display);
+        //mRadiusDisplay = (TextView) findViewById(R.id.radius_display);
         mRadiusSeekbar = (SeekBar) findViewById(R.id.seekbar_radius);
         //mRecyclerView = (RecyclerView) findViewById(R.id.limit_recycler_view);
         mLayoutManager = new LinearLayoutManager(this, LinearLayout.VERTICAL, false);
